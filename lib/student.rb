9cf0e2df77c44e7cac35178d30562d96a91e4c4e
Paul Rosero <paul_rosero@yahoo.com>
2019-07-14 03:41:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-  en
+  end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
