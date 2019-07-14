834aba2479ffda779a021a28cdccaf59c5854f5f
Paul Rosero <paul_rosero@yahoo.com>
2019-07-14 03:45:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,9 @@ class Student
   end
   
   def self.new_from_db(array)
-    
-    student.id = array[0]
-    student.name = array[1]
-    student.grade = array[2]
+    id = array[0]
+    name = array[1]
+    grade = array[2]
     student = self.new(name, grade, id)
   end
   
